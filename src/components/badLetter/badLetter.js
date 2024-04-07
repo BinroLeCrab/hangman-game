ef4d4710b61071c2c2842565dbd10cb905eefee6
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-04-07 22:21:53 +0200

    💄 UI Bad Letter

--- src/components/badLetter/badLetter.js
+++ src/components/badLetter/badLetter.js
@@ -1,10 +1,14 @@
+import './badLetter.css';
+
 export const BadLetter = ({ lettresWrong }) => {
     return (
-        <div>
-            <h3>Wrong letters</h3>
+        <div className="badLetter__container">
+            <h2 className="title badLetter__title">Mauvaises Lettres</h2>
+            <div className="badLetter__content">
             {lettresWrong.map((letter, index) => {
-                return <span key={index}>{letter} </span>
+                return <span className="badLetter__object" key={index}>{letter} </span>
             })}
+            </div>
         </div>
     );
 }
